3d5a4c216afbf26bbbed4804f6ca2283c7253c4f
Facundo Baquero <facundito.xB@gmail.com>
2021-11-01 21:25:10 -0300

    Ruteo de ejercicios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
 import Ejercicio1 from './components/Ejercicio.jsx';
 import Ejercicio2 from './components/Ejercicio2.jsx';
+import Main from './components/main.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <Ejercicio1/>
-      <Ejercicio2/>
-    </div>
+    <Router>
+      <div className="App">
+      <Main></Main>
+        <Switch>
+          <Route path="/ejercicio1" exact>
+            <Ejercicio1></Ejercicio1>
+          </Route>
+          <Route path="/ejercicio2" exact>
+            <Ejercicio2></Ejercicio2>
+          </Route>            
+        </Switch>
+      </div>
+
+    </Router>
+
   );
 }
 
---
+++ src/components/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';  
+
+
+const Main =() => {
+    return (
+        <>
+            <div>
+                <a href="/ejercicio1">Ejercicio primero</a>
+                <a href="/ejercicio2">Ejercicio Segundo</a>
+                <a href="/ejercicio3">Ejercicio tre</a>
+                <a href="/ejercicio4">El ultimo ejercicio</a>
+            </div>  
+        </>
+    )
+}
+
+export default Main
